eb5b0cdad2049495a42979e5c1d52e8f362b7dff
Mehmet Bulut <33868627+mehmetbltt23@users.noreply.github.com>
2022-10-23 16:14:32 +0300

    fyne package install

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Hello")
+	w.Resize(fyne.Size{500, 500})
+	w.CenterOnScreen()
+	w.Hide()
+	hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome :)")
+		}),
+	))
+
+	w.ShowAndRun()
+}
